d814654fa9afa435bf4e5021d198c836b95fcff1
Marcelo França <marcelo.frneves@gmail.com>
2019-06-03 20:25:16 +0000

    Chef Version update

--- ubuntu-18.04/scripts/getchef.sh
+++ ubuntu-18.04/scripts/getchef.sh
@@ -15,8 +15,10 @@
 #    v1.0 2019-03-18, Marcelo Franca:
 #       - Arranging shell script "docker-entrypoint.sh" to other script
 #         named "getchef.sh". This script can only download chef
-sha256="a95d248052c456fc5320eefbfe501c7e1282c029569fad5967f10f400e5a86d9"
-chef_url="https://packages.chef.io/files/stable/chef-server/12.19.26/ubuntu/18.04/chef-server-core_12.19.26-1_amd64.deb"
+#    v1.1 2019-06-03, Marcelo Franca:
+#	- Chef Version update.
+sha256="bbf6127e03d10154e28b1270869731b38bd5a0981c9f9cb96f973c290d14c4df"
+chef_url="https://packages.chef.io/files/stable/chef-server/12.19.31/ubuntu/18.04/chef-server-core_12.19.31-1_amd64.deb"
 
 if [[ $sha256 == $(wget --no-check-certificate $chef_url -O $LOCAL_FILE && \
 	sha256sum $LOCAL_FILE | awk '{print $1}') ]]; then
